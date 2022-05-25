e4fc09658074434bb5d41dcb9b05d00b887b8eae
Bulalu <elishabulalu031@gmail.com>
2022-05-25 15:41:23 +0300

    ✨ spiced up

--- contracts/CharityDAO.sol
+++ contracts/CharityDAO.sol
@@ -33,7 +33,7 @@ contract CharityDAO is ReentrancyGuard, AccessControl {
         
     }
 
-    mapping(uint256 => CharityProposal) private charityProposal;
+    mapping(uint256 => CharityProposal) private charityProposals;
     mapping(address => uint256[]) private stakeholderVotes;
     mapping(address => uint256) private contributors;
     mapping(address => uint256) private stakeHolders;
@@ -69,7 +69,7 @@ contract CharityDAO is ReentrancyGuard, AccessControl {
             numOfProposals.increment();
             uint256 proposalId = numOfProposals.current();
             
-            CharityProposal storage proposal = charityProposal[proposalId];
+            CharityProposal storage proposal = charityProposals[proposalId];
             proposal.id = proposalId;
             proposal.description = description;
             proposal.charityAddress = payable(charityAddress);
@@ -80,10 +80,80 @@ contract CharityDAO is ReentrancyGuard, AccessControl {
 
         }
 
-    function vote(uint256 proposalId, bool supportProposal) external onlyStakeHolder("Only stake holders are allowed to vote") {
+    function vote(uint256 proposalId, bool supportProposal) external onlyStakeholder("Only stake holders are allowed to vote") {
         
         CharityProposal storage charityProposal = charityProposals[proposalId];
         
-        votable(charityProposal)
+        votable(charityProposal);
+
+        if (supportProposal) charityProposal.votesFor++;
+        else charityProposal.votesAgainst++;
+
+        stakeholderVotes[msg.sender].push(charityProposal.id);
+
+    }
+
+    function votable(CharityProposal storage charityProposal) private {
+        if(
+            charityProposal.votingPassed || charityProposal.livePeriod <= block.timestamp
+        ) {
+            charityProposal.votingPassed = true;
+            revert("Voting period has passed on this proposal");
+        }
+
+        uint256[] memory Votes = stakeholderVotes[msg.sender];
+        for(uint256 index=0; index < Votes.length; index++) {
+            if(charityProposal.id == Votes[index]) {
+                revert("You have already voted on this proposal");
+            }
+        }
+    }
+
+    // must it be after the voting period has ended?
+    // one time thing?
+    // only admin?
+    function payCharity(uint256 proposalId) external onlyStakeholder("Only stakeholders are allowed to make payments") {
+
+        CharityProposal storage charityProposal = charityProposals[proposalId];
+
+        if(charityProposal.paid) revert("Payment has already been made to this charity");
+
+        if( charityProposal.votesFor <= charityProposal.votesAgainst) 
+            revert("The proposal does not enough votes to be paid for");
+
+        charityProposal.paid = true;
+        charityProposal.paidBy = msg.sender;
+
+        emit PaymentTransfered(msg.sender, charityProposal.charityAddress, charityProposal.amount);
+
+
+        return charityProposal.charityAddress.transfer(charityProposal.amount);
+
+    }
+
+    receive() external payable {
+        emit ContributionReceived(msg.sender, msg.value);
+    }
+
+    function makeStakeholder(uint256 amount) external {
+        address account = msg.sender;
+        uint256 amountContributed = amount;
+
+        // think more 
+        if (!hasRole(STAKEHOLDER_ROLE, account)) {
+            uint256 totalContributed = contributors[account] + amountContributed;
+
+            if (totalContributed >= 5 ether) {
+                stakeHolders[account] = totalContributed;
+                contributors[account] += amountContributed;
+                _setupRole(STAKEHOLDER_ROLE, account);
+                _setupRole(CONTRIBUTER_ROLE, account);
+            } else {
+                contributors[account] += amountContributed;
+                _setupRole(CONTRIBUTER_ROLE, account);
+            }
+        }
+    }
+
     
 }
