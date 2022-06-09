3e8d0a5c3519e0b439cbedc6721ecc23005a2539
Bulalu <elishabulalu031@gmail.com>
2022-06-09 15:51:28 +0300

    looking pretty now

--- frontend/my-app/src/pages/Home.js
+++ frontend/my-app/src/pages/Home.js
@@ -1,75 +1,183 @@
 import React, { useEffect, useState } from "react";
 import "./pages.css";
-import { TabList, Tab, Widget, Tag, Table, Form} from "web3uikit";
+import { TabList, Tab, Widget, Tag, Table, Form } from "web3uikit";
 import { Link } from "react-router-dom";
-
-
+import { useMoralis, useMoralisWeb3Api, useWeb3ExecuteFunction } from "react-moralis";
 
 const Home = () => {
- 
+  const [passRate, setPassRate] = useState(0);
+  const [totalP, setTotalP] = useState(0);
+  const [counted, setCounted] = useState(0);
+  const [voters, setVoters] = useState(0);
+  const { Moralis, isInitialized } = useMoralis();
+  const [proposals, setProposals] = useState();
+  const Web3Api = useMoralisWeb3Api();
+  const [sub, setSub] = useState();
+  const contractProcessor = useWeb3ExecuteFunction();
 
 
-const [proposals, setProposals] = useState([
-    [
-      1,
-      <div>Should we start a Moralis hamburger chain?</div>,
-      <Tag color="green" text="Passed" />,
-    ],
-    [
-      2,
-      "Should we accept Elon Musks $44billion offer for our DAO?",
-      <Link to="/proposal" state={"hello"}>
-        <Tag color="red" text="Rejected" />
-      </Link>,
-    ],
-    [
-      3,
-      "Do you want a Web3 Slack tutorial?",
-      <Tag color="blue" text="Ongoing" />,
-    ],
-    [
-      4,
-      "Are you interested in Xbox/Console web3 tutorials?",
-      <Tag color="blue" text="Ongoing" />,
-    ],
-    [
-      5,
-      "Would you attend a Moralis Builder get together in Miami?",
-      <Tag color="blue" text="Ongoing" />,
-    ],
-]);
- 
+  async function createProposal(newProposal) {
+    let options = {
+      contractAddress: "0xF304Ddf294d05c80995FB0702b40DfEA8E48582a",
+      functionName: "createProposal",
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_description",
+              type: "string",
+            },
+            {
+              internalType: "address[]",
+              name: "_canVote",
+              type: "address[]",
+            },
+          ],
+          name: "createProposal",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      params: {
+        _description: newProposal,
+        _canVote: voters,
+      },
+    };
+
+
+    await contractProcessor.fetch({
+      params: options,
+      onSuccess: () => {
+        console.log("Proposal Succesful");
+        setSub(false);
+      },
+      onError: (error) => {
+        alert(error.data.message);
+        setSub(false);
+      },
+    });
+
+
+  }
+
+
+  async function getStatus(proposalId) {
+    const ProposalCounts = Moralis.Object.extend("ProposalCounts");
+    const query = new Moralis.Query(ProposalCounts);
+    query.equalTo("uid", proposalId);
+    const result = await query.first();
+    if (result !== undefined) {
+      if (result.attributes.passed) {
+        return { color: "green", text: "Passed" };
+      } else {
+        return { color: "red", text: "Rejected" };
+      }
+    } else {
+      return { color: "blue", text: "Ongoing" };
+    }
+  }
+
+  useEffect(() => {
+    if (isInitialized) {
+
+      async function getProposals() {
+        const Proposals = Moralis.Object.extend("Proposals");
+        const query = new Moralis.Query(Proposals);
+        query.descending("uid_decimal");
+        const results = await query.find();
+        const table = await Promise.all(
+          results.map(async (e) => [
+            e.attributes.uid,
+            e.attributes.description,
+            <Link to="/proposal" state={{
+              description: e.attributes.description,
+              color: (await getStatus(e.attributes.uid)).color,
+              text: (await getStatus(e.attributes.uid)).text,
+              id: e.attributes.uid,
+              proposer: e.attributes.proposer
+              
+              }}>
+              <Tag
+                color={(await getStatus(e.attributes.uid)).color}
+                text={(await getStatus(e.attributes.uid)).text}
+              />
+            </Link>,
+          ])
+        );
+        setProposals(table);
+        setTotalP(results.length);
+      }
+
+
+      async function getPassRate() {
+        const ProposalCounts = Moralis.Object.extend("ProposalCounts");
+        const query = new Moralis.Query(ProposalCounts);
+        const results = await query.find();
+        let votesUp = 0;
+
+        results.forEach((e) => {
+          if (e.attributes.passed) {
+            votesUp++;
+          }
+        });
+
+        setCounted(results.length);
+        setPassRate((votesUp / results.length) * 100);
+      }
+
+
+      const fetchTokenIdOwners = async () => {
+        const options = {
+          address: "0x705f8B395361218056B20eE5C36853AB84b8bbFF",
+          token_id:
+            "0",
+          chain: "rinkeby",
+        };
+        const tokenIdOwners = await Web3Api.token.getTokenIdOwners(options);
+        const addresses = tokenIdOwners.result.map((e) => e.owner_of);
+        setVoters(addresses);
+      };
+
+
+      fetchTokenIdOwners();
+      getProposals();
+      getPassRate();
+      
+    }
+  }, [isInitialized]);
+
 
   return (
     <>
       <div className="content">
-          <TabList defaultActiveKey={1} tabStyle="bulbUnion">
-              <Tab tabKey={1} tabName="DAO">
-                  <div className="tabContent">
-                      Governance Overview
-                      <div className="widgets">
-                           <Widget
-                            info={52}
-                            title="Proposals Created"
-                            style={{ width: "200%"}}
-                            >
-                             <div className="extraWidgetInfo">
-                                <div className="extraTitle">Pass Rate</div>
-                                <div className="progress">
-                                <div
-                                    className="progressPercentage"
-                                    style={{ width: `${30}%` }}
-                                ></div>
-                                </div>
-                            </div>    
-                            </Widget> 
-                            
-                            <Widget info={432} title="Eligible Voters" /> 
-                            <Widget info={5} title="Ongoing Proposals" /> 
- 
-                      </div>
-                      Recent Proposals
-                      <div style={{ marginTop: "30px" }}>
+        <TabList defaultActiveKey={1} tabStyle="bulbUnion">
+          <Tab tabKey={1} tabName="DAO">
+            {proposals && (
+            <div className="tabContent">
+              Governance Overview
+              <div className="widgets">
+                <Widget
+                  info={totalP}
+                  title="Proposals Created"
+                  style={{ width: "200%" }}
+                >
+                  <div className="extraWidgetInfo">
+                    <div className="extraTitle">Pass Rate</div>
+                    <div className="progress">
+                      <div
+                        className="progressPercentage"
+                        style={{ width: `${passRate}%` }}
+                      ></div>
+                    </div>
+                  </div>
+                </Widget>
+                <Widget info={voters.length} title="Eligible Voters" />
+                <Widget info={totalP-counted} title="Ongoing Proposals" />
+              </div>
+              Recent Proposals
+              <div style={{ marginTop: "30px" }}>
                 <Table
                   columnsConfig="10% 70% 20%"
                   data={proposals}
@@ -81,38 +189,39 @@ const [proposals, setProposals] = useState([
                   pageSize={5}
                 />
               </div>
-                   
-                    <Form
-                        buttonConfig={{
-                            isLoading: false,
-                            loadingText: "Submitting Proposal",
-                            text: "Submit",
-                            theme: "secondary",
-                        }}
-                        data={[
-                            {
-                            inputWidth: "100%",
-                            name: "New Proposal",
-                            type: "textarea",
-                            validation: {
-                                required: true,
-                            },
-                            value: "",
-                            },
-                        ]}
-                        onSubmit={(e) => {
-                            alert("Proposal Submitted")
-                        }}
-                        title="Create a New Proposal"
-                    />
 
-                  </div>
-                  
-                  
-              </Tab>
-              <Tab tabKey={2} tabName="Forum"></Tab>
-              <Tab tabKey={3} tabName="Docs"></Tab>
-          </TabList>
+              <Form
+                  buttonConfig={{
+                    isLoading: sub,
+                    loadingText: "Submitting Proposal",
+                    text: "Submit",
+                    theme: "secondary",
+                  }}
+                  data={[
+                    {
+                      inputWidth: "100%",
+                      name: "New Proposal",
+                      type: "textarea",
+                      validation: {
+                        required: true,
+                      },
+                      value: "",
+                    },
+                  ]}
+                  onSubmit={(e) => {
+                    setSub(true);
+                    createProposal(e.data[0].inputResult);
+                  }}
+                  title="Create a New Proposal"
+                />
+
+
+            </div>
+            )}
+          </Tab>
+          <Tab tabKey={2} tabName="Forum"></Tab>
+          <Tab tabKey={3} tabName="Docs"></Tab>
+        </TabList>
       </div>
       <div className="voting"></div>
     </>
