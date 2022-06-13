11f3022c6444a2d5eb6fd5e52510bf07ebd20c0f
Bulalu <elishabulalu031@gmail.com>
2022-06-13 17:40:11 +0300

    🍱 UI baby

--- scripts/deploy_focus_dao.py
+++ scripts/deploy_focus_dao.py
@@ -15,7 +15,21 @@ def deploy():
         print("Here's the Already deployed contract!")
         return FocusDao[-1]
 
+def create_proposal():
+    account = get_account()
+
+    address_list = ["0xA66F90C0B7be6955D6c8f9B16dfD0A56171e038e"]
+    description = "Create NFT Music Concert as our next project"
+
+    contract = deploy()
+
+    print("Woop Woop!, creating new proposal captain")
+    contract.createProposal(description, address_list, {"from":account})
+
 def main():
-    deploy()
+    # deploy()
+    # create_proposal()
+    print(FocusDao[-1])
 
     #0x10D736D75ba0d9A937DFf4c8585db4CDD3A3C458
+    # 0xD2576Ea24200b90eC58c9c8472469916A1592CeF
